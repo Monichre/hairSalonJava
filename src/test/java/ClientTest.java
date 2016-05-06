c39fa3bfb187cf1ca20ebc1568d22c04cf96e9de
Liam Ellis <liamhellis@gmail.com>
2016-05-05 21:56:51 -0700

    before and after tests that are up and running

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -4,6 +4,19 @@ import static org.junit.Assert.*;
 
 public class ClientTest {
 
+	@Before
+	public void setUp() {
+		DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+	}
+
+	@After
+	public void tearDown() {
+		try(Connection con = DB.sql2o.open()) {
+			String sql = "DELETE FROM clients *;";
+			con.createQuery(sql).executeUpdate();
+		}
+    }
+
 	@Test
 	public void clientsAreTheSame() {
 		Client client1 = new Client("Bill");
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -4,6 +4,21 @@ import static org.junit.Assert.*;
 
 public class StylistTest {
 
+
+
+	@Before
+	public void setUp() {
+		DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+	}
+
+	@After
+	public void tearDown() {
+		try(Connection con = DB.sql2o.open()) {
+			String sql = "DELETE FROM stylists *;";
+			con.createQuery(sql).executeUpdate();
+		}
+    }
+
 	@Test
 	public void stylistsAreTheSame_True() {
 		Stylist stylist1 = new Stylist("Sarah");
